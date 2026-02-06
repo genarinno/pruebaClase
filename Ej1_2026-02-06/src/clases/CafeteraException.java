44484345b0538dfada09bb1d8fb72b2d5ae1e559
Genarinno <jsbuenos@gmail.com>
2026-02-06 12:45:36 +0100

    Ejercicio Cafetera terminado - Javier Sanchez

---
+++ Ej1_2026-02-06/src/clases/CafeteraException.java
@@ -0,0 +1,17 @@
+package clases;
+
+
+@SuppressWarnings("serial")
+public class CafeteraException extends Exception
+{
+	public CafeteraException(String pMensaje)
+	{
+		super(pMensaje);
+	}
+	
+	public void escribirEnFichero()
+	{
+		// Instrucciones para añadir texto en log
+	}
+	
+}
