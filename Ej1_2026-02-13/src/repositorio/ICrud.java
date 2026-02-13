5cb69c7e3cd6ca2b74c583796781354a4c3bf7cb
Genarinno <jsbuenos@gmail.com>
2026-02-13 14:07:22 +0100

    Ejemplo CRUD BBDD Producto 2026-02-13.

---
+++ Ej1_2026-02-13/src/repositorio/ICrud.java
@@ -0,0 +1,16 @@
+package repositorio;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public interface ICrud<PK, MODELO>
+{
+	public abstract MODELO leer(PK id) throws Exception;
+	public abstract ArrayList<MODELO> listar() throws Exception;
+	public abstract boolean nuevo(MODELO modelo) throws SQLException;
+	public abstract boolean editar(PK id, MODELO modelo) throws Exception;
+	
+	public abstract boolean eliminar(PK id) throws Exception;
+	public abstract boolean eliminarModelo(MODELO modelo) throws Exception;
+
+}
